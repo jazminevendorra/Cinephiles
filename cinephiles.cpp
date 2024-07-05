49c8e3c8b477e123328f40da0b8d0cec77f972d6
Jazmin <163955648+jazminevendorra@users.noreply.github.com>
2024-07-05 11:27:24 -0400

    Update cinephiles.cpp

--- cinephiles.cpp
+++ cinephiles.cpp
@@ -1,11 +1,9 @@
 /*
-Jazmin Even Dorra
 
 cinephiles.cpp
 
 This program will ask user for file containing the amount of movies seen by students in different faculties,
 display the number of movies seen by number of students, and display the average movies seen.
-
 
 */
 
